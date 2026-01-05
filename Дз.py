85341b7511260927189c44390f65eba2145f396d
Oleksandr <miskodmytro2012@gmail.com>
2026-01-05 13:35:02 +0200

    Дз

---
+++ Дз.py
@@ -0,0 +1,37 @@
+a = 1
+b = 10
+
+x1 = a
+x2 = a + 1
+x3 = a + 2
+x4 = a + 3
+x5 = a + 4
+x6 = a + 5
+x7 = a + 6
+x8 = a + 7
+x9 = a + 8
+x10 = b
+
+print(f"{x1}\n", end="")
+print(f"{x2}\n", end="")
+print(f"{x3}\n", end="")
+print(f"{x4}\n", end="")
+print(f"{x5}\n", end="")
+print(f"{x6}\n", end="")
+print(f"{x7}\n", end="")
+print(f"{x8}\n", end="")
+print(f"{x9}\n", end="")
+print(f"{x10}\n", end="")
+
+y1 = x1
+y2 = x2
+y3 = x3
+y4 = x4
+y5 = x5
+y6 = x6
+y7 = x7
+y8 = x8
+y9 = x9
+y10 = x10
+
+z = y1 + y2 + y3 + y4 + y5 + y6 + y7 + y8 + y9 + y10
